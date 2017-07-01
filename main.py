b0d26440490a44413c4b09a8a777e0629776a469
Jun Ito <jun_ito@voyagegroup.com>
2017-07-01 16:51:40 +0900

    Update: 抽象化した

--- main.py
+++ main.py
@@ -4,41 +4,32 @@
 import RPi.GPIO as GPIO
 import time
 
-room1 = {'pin': 18, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
-room2 = {'pin': 15, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
-loopDuration = 1
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(room1['pin'], GPIO.IN)
 GPIO.setup(room2['pin'], GPIO.IN)
 
+room1 = {'pin': 18, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+room2 = {'pin': 15, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+loopDuration = 1
+
 while(True):
     time.sleep(loopDuration)
-
     needChangeStatus = False
-    room1['history'].pop(0)
-    room1['history'].append(GPIO.input(room1['pin']))
-    room2['history'].pop(0)
-    room2['history'].append(GPIO.input(room1['pin']))
 
-    if all(x == 0 for x in room1['history']) and room1['status'] = 0:
-        needChangeStatus = True
-        room1['status'] = 0
+    for room in [room1, room2]:
+        room['history'].pop(0)
+        room['history'].append(GPIO.input(room['pin']))
 
-    if all(x == 1 for x in room1['history']) and room1['status'] = 1:
-        needChangeStatus = True
-        room1['status'] = 1
+        if all(x == 0 for x in room['history']) and room['status'] = 0:
+            needChangeStatus = True
+            room['status'] = 0
 
-    if all(x == 0 for x in room2['history']) and room2['status'] = 0:
-        needChangeStatus = True
-        room2['status'] = 0
-
-    if all(x == 1 for x in room2['history']) and room2['status'] = 1:
-        needChangeStatus = True
-        room2['status'] = 1
+        if all(x == 1 for x in room['history']) and room['status'] = 1:
+            needChangeStatus = True
+            room['status'] = 1
 
     if needChangeStatus:
-        Slack.send(room1['status'], room2['status'])
-
+        # Slack.send(room1['status'], room2['status'])
+        print room1['status'] + ' ' + room2['status']
 
 GPIO.cleanup()
