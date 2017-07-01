0e19a0ef6e54d1bd27506730922fc55c40d551c7
Jun Ito <jun_ito@voyagegroup.com>
2017-07-01 16:36:34 +0900

    Update: 10秒連続で同じ値だったら値を書き換えるように変更

--- main.py
+++ main.py
@@ -4,11 +4,28 @@
 import RPi.GPIO as GPIO
 import time
 
+room1 = {'pin': 18, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+# room2 = {'pin': 15, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+loopDuration = 1
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(room1['pin'], GPIO.IN)
+GPIO.setup(room2['pin'], GPIO.IN)
 
 while(True):
-	print GPIO.input(18)
-	time.sleep(0.2)
+    room1['history'].pop(0)
+    room1['history'].append(GPIO.input(room1['pin']))
+
+    if all(x == 0 for x in room1['history']):
+        new_status = 0
+
+    if all(x == 1 for x in room1['history']):
+        new_status = 1
+
+    if room1['status'] == new_status:
+        print 'status changed'
+        room1['status'] = new_status
+
+    time.sleep(loopDuration)
 
 GPIO.cleanup()
