f9ea99200b18e5bab3ac78c1173f52b603c0f16a
Jun Ito <jun_ito@voyagegroup.com>
2017-07-01 17:30:54 +0900

    Update: 表記などを修正

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
+from slack import Slack
+Slack = Slack()
 
-room1 = {'pin': 18, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
-room2 = {'pin': 27, 'status': 0, 'history': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
-loopDuration = 1
+room1 = {'pin': 18, 'status': 0, 'history': [0] * 10}
+room2 = {'pin': 27, 'status': 0, 'history': [0] * 10}
+loop_duration = 1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(room1['pin'], GPIO.IN)
 GPIO.setup(room2['pin'], GPIO.IN)
 
 while(True):
-    time.sleep(loopDuration)
-    needChangeStatus = False
+    time.sleep(loop_duration)
+    need_change_status = False
 
     for room in [room1, room2]:
         print room['history']
@@ -22,11 +24,10 @@ while(True):
         room['history'].append(GPIO.input(room['pin']))
 
         if all(x != room['status'] for x in room['history']):
-            needChangeStatus = True
+            need_change_status = True
             room['status'] = 1 - room1['status']
 
-    if needChangeStatus:
-        # Slack.send(room1['status'], room2['status'])
-        print str(room1['status']) + ' ' + str(room2['status'])
+    if need_change_status:
+        Slack.post_status(bool(room1['status']), bool(room2['status']))
 
 GPIO.cleanup()
