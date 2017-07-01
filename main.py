7c9bd6b7b8595ec244f67296c4276a60c05576f0
Jun Ito <jun_ito@voyagegroup.com>
2017-07-01 20:03:00 +0900

    Update: コメントをさらに詳しく

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 import RPi.GPIO as GPIO
 import time
 from slack import Slack
-Slack = Slack()
 
-# room['status']は現在Slack上で表示されているはずの状態
-# room['history']は過去10回分のセンサーの取得値を格納する配列
+# room['pin'] → raspberry pi 上で利用するピン番号
+# room['status'] → 現在の部屋の状態(0: 利用不可能, 1: 利用可能)
+# room['history'] → 過去10回分のセンサーの取得値を格納する配列
 room1 = {'pin': 18, 'status': 0, 'history': [0] * 10}
 room2 = {'pin': 27, 'status': 0, 'history': [0] * 10}
+
 loop_duration = 1
+Slack = Slack()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(room1['pin'], GPIO.IN)
@@ -26,7 +28,7 @@ while(True):
         room['history'].pop(0)
         print room['history']
 
-        # history の全てのステータスが、現在保存されているステータスと逆だったらSlackステータスを変更
+        # history の全てのステータスが、現在保存されているステータスと異なっていたらSlackステータスを変更
         if all(x != room['status'] for x in room['history']):
             should_change_status = True
             room['status'] = 1 - room1['status']
