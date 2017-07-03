0d5a67d029529363deda6ef570a737c099551c64
Jun Ito <jun_ito@voyagegroup.com>
2017-07-03 11:31:22 +0900

    Update: 分かりやすいように変数名などを変更

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import time
 from slack import Slack
 
 # room['pin'] → raspberry pi 上で利用するピン番号
-# room['status'] → 現在の部屋の状態(0: 利用不可能, 1: 利用可能)
+# room['status'] → 現在の部屋のステータス(0: 利用不可能, 1: 利用可能)
 # room['history'] → 過去10回分のセンサーの取得値を格納する配列
 room1 = {'pin': 18, 'status': 0, 'history': [0] * 10}
 room2 = {'pin': 27, 'status': 0, 'history': [0] * 10}
 
 loop_duration = 1
-Slack = Slack()
+slack = Slack()
 
 # GPIO（ラズパイの汎用IOピンの初期化）
 GPIO.setmode(GPIO.BCM)
@@ -25,16 +25,16 @@ while(True):
 
     for room in [room1, room2]:
         # 近接センサーの取得値は、0(近接あり) と 1（近接なし）の2値
-        room['history'].append(GPIO.input(room['pin']))
-        room['history'].pop(0)
+        room['history'].append(GPIO.input(room['pin']))  # 最新の値を末尾に追加
+        room['history'].pop(0)  # 一番古い履歴を削除
         print room['history']
 
-        # history の全てのステータスが、現在保存されているステータスと異なっていたらSlackステータスを変更
-        if all(x != room['status'] for x in room['history']):
+        # history の全てのステータスが、現在保存されているステータスと異なっていたらSlackステータスを更新
+        if all(status != room['status'] for status in room['history']):
             should_change_status = True
-            room['status'] = 1 - room['status']
+            room['status'] = room['history'][0]
 
     if should_change_status:
-        Slack.post_status(bool(room1['status']), bool(room2['status']))
+        slack.post_status(bool(room1['status']), bool(room2['status']))
 
 GPIO.cleanup()
