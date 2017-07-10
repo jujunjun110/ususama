7d3cad6a449858e39f80b3b82725ad755a6122e1
ususama raspberry pi <jun_ito@voyagegroup.com>
2017-07-10 11:11:29 +0900

    Add: startup.sh をgithub管理
    Merge branch 'master' of github.com:jujunjun110/ususama

---
+++ startup.sh
@@ -0,0 +1,7 @@
+LIVING_PROCESS=`ps ux | grep -c [m]ain.py`
+
+if [ ${LIVING_PROCESS} -eq 0 ];then
+  echo 'START EXECUTION'
+  cd /home/pi/ususama/
+  nohup python main.py > /home/pi/ususama/log/log.txt &
+fi
