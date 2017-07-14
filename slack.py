71fba58ddcab04f54c4e20bed5bffcf78dbe5cf8
Jun Ito <jun_ito@voyagegroup.com>
2017-07-14 18:04:13 +0900

    Update: ステータスポスト時に時間を入れるように変更

--- slack.py
+++ slack.py
@@ -2,23 +2,27 @@
 import requests
 import yaml
 import logging
+from datetime import datetime
 
 token_path = './token-file.yml'
 token_file = open(token_path)
 token = yaml.load(token_file)['token']
 logger = logging.getLogger('logger.main')
 
+
 class Slack:
     def post_status(self, room1, room2):
 
         emoji = self.decide_status(room1, room2)
-        profile = '{"status_emoji": "%s"}' % emoji
-        response = requests.post('https://slack.com/api/users.profile.set', params = {
+        message = 'Last Update: %s' % datetime.now().strftime('%H:%M')
+
+        profile = '{"status_emoji": "%s", "status_text": "%s"}' % (emoji, message)
+        response = requests.post('https://slack.com/api/users.profile.set', params={
             'token': token,
             'profile': profile
         })
 
-	logger.debug(response.json())
+        logger.debug(response.json())
 
     def decide_status(self, room1, room2):
         if room1 and room2:
